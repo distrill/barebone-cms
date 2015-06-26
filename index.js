1d552a639a20ce0c31dcf0777697fa93c163bfa7
distrill <brent@arthurkn.net>
2015-06-26 00:54:40 -0700

    bug fix- get rid of ejs all together (for now)

--- index.js
+++ index.js
@@ -10,19 +10,16 @@ module.exports.initialize = function( app ) {
         extended: true
     }));
     app.use( bodyParser.json() );
-
-    app.set( 'views', __dirname +'/views' );
-    app.set( 'view engine', 'ejs' );
 };
 
 module.exports.renderNewCat = function( req, res ) {
-    res.render( 'index', {} );
+    res.sendFile( __dirname + '/views/index.html' );
 }
 
-module.exports.createNewCat = function( cat ) {
-    var kitty = new Cat( { name: cat.name });
+module.exports.createNewCat = function( req, res ) {
+    var kitty = new Cat( { name: req.body.name });
     kitty.save( function( err ) {
         if( err ) console.log( err );
-        console.log( 'new motherfucking cat like a motherfucker' );
+        res.redirect( '/' );
     });
 }
