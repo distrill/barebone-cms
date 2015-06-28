727e7d6d983047ae4b8a7002bcd57f334ac7c0bb
distrill <brent@arthurkn.net>
2015-06-28 11:48:16 -0700

    abstract initialization, moved from index.js to config/init.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-var bodyParser = require( 'body-parser' ),
-    mongoose = require( 'mongoose' ),
-    multer = require( 'multer' ),
-    config = require( './config/settings' );
-
-module.exports.initialize = function( app ) {
-    mongoose.connect( config.db );
-    app.use( multer( config.multer ));
-    app.use( bodyParser.urlencoded( {
-        extended: true
-    }));
-    app.use( bodyParser.json() );
-};
+module.exports.initialize = require( './lib/init.js' );
 
 module.exports.cats = require( './lib/cats.js' );
-
 module.exports.articles = require( './lib/articles.js' );
